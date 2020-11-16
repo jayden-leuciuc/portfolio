da41854498fe48ae648a02d3915ebe3bc66932e4
Jayden Leuciuc <leuciucjayden@gmail.com>
2020-11-16 06:42:30 -0500

    Created first page

---
+++ src/Components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.scss'
+function Hero() {
+    return (
+        <div className="hero-container">
+            <div className="text-container">
+                <h1>Jayden Leuciuc</h1>
+                <p>Self-taught web developer</p>
+            </div>  
+        </div>
+    )
+}
+
+export default Hero
