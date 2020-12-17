da0d190bb106927d8cfdc9d261a0e1d4b140db65
jayden leuciuc <leuciucjayden@gmail.com>
2020-12-17 14:33:23 -0500

    Added project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import Card from '../Components/Card'
-import {projectOne, projectTwo} from './Data'
-import './Projects.scss'
+import React from 'react';
+import Card from '../Components/Card';
+import { projectOne, projectTwo, projectThree } from './Data';
+import './Projects.scss';
 
 function Projects() {
-    return (
-       <div className="projects-Header">
-           <h1>&lt;Projects /&gt;</h1>
-        <div className="projects-container">
-            
-            <Card {...projectOne}/>
-            <Card {...projectTwo}/>
-        </div>
+  return (
+    <div className='projects-Header'>
+      <h1>&lt;Projects /&gt;</h1>
+      <div className='projects-container'>
+        <Card {...projectThree} />
+        <Card {...projectTwo} />
+        <Card {...projectOne} />
+      </div>
     </div>
-    )
+  );
 }
 
-export default Projects
+export default Projects;
