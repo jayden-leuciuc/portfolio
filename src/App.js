a5709c4ee3745b7a01f244866f979ad180e99415
jayden leuciuc <leuciucjayden@gmail.com>
2020-12-19 09:52:18 -0500

    Added github commit calendar component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './app.scss';
 import About from './Components/About';
 import Hero from './Components/Hero';
 import Projects from './Components/Projects';
+import Github from './Components/Github';
 
 function App() {
   return (
     <>
-    <Hero /> 
-    <About />
-    <Projects />
-
+      <Hero />
+      <About />
+      <Github />
+      <Projects />
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/Components/Github.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Github.scss';
+
+function Github() {
+  return (
+    <div className='githubDiv'>
+      <h1>Github Calendar:</h1>
+      <img
+        className='gitHubImg'
+        src='http://ghchart.rshah.org/107e7d/jayden-leuciuc'
+        alt='GitHub calendar'
+      ></img>
+    </div>
+  );
+}
+
+export default Github;
