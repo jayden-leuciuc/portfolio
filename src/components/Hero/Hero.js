1c4c49b983a935cd75e9404a3fedee1a8d95493b
Jayden Leuciuc <leuciucjayden@gmail.com>
2022-08-10 23:24:43 -0400

    feat: Add custom react components

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Hero.scss"
+
+function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="text-container">
+        <h1 className="hero-intro">Hi, I'm</h1>
+        <h1 className="hero-name">Jayden Leuciuc</h1>
+        <p className="hero-description">Web Developer</p>
+      </div>
+      {/*<div className="icon-container">
+        <AiOutlineArrowDown className="iconArrow" /> 
+      </div>*/}
+    </div>
+  )
+}
+
+export default Hero
