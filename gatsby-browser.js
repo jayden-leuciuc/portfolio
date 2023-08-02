5a744357bcf73726900e56269489353ecaee55da
jayden-leuciuc <leuciucjayden@gmail.com>
2023-08-02 17:52:11 -0400

    chore: Delete files

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-// normalize CSS across browsers
-import "./src/styles/main.scss"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
